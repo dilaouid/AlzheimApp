d2fbf0e83f3618b1a973bdcf3a5f1dd096fb0818
Yedine <yedine@live.fr>
2021-12-04 16:26:55 +0100

    (+) Init Models

---
+++ src/models/Person.ts
@@ -0,0 +1,12 @@
+/*  The model of the tracked person with the Alzeihmer disease. 
+    Each individual would have his/her Person model attached to.
+ */
+
+import { Activity } from './Activity';
+
+export interface Person {
+    id: string; /* uuidv4 */
+    name: string; /* the name of the person */
+    picture: number; /* the picture ID */
+    activity: Activity[];
+};
