575a75706ebfd37bd5416047fa1a946aadc75c10
Yedine <yedine@live.fr>
2021-12-04 16:28:11 +0100

    (+) Initialize data content

---
+++ src/data/api.ts
@@ -0,0 +1,51 @@
+import { Plugins } from '@capacitor/core';
+
+const { Storage } = Plugins;
+
+const dataUrl = '/assets/data/data.json';
+const USERNAME = 'username';
+const HAS_LOGGED_IN = 'hasLoggedIn';
+const HAS_SEEN_TUTORIAL = 'hasSeenTutorial';
+
+export const getConfData = async () => {
+    const response = await Promise.all([
+      fetch(dataUrl)
+    ]);
+    const responseData = await response[0].json();
+    const data = {
+        responseData
+    };
+    return data ?? [];
+};
+
+export const getUserData = async () => {
+    const response = await Promise.all([
+      Storage.get({ key: HAS_LOGGED_IN }),
+      Storage.get({ key: HAS_SEEN_TUTORIAL }),
+      Storage.get({ key: USERNAME })]);
+    const isLoggedin = await response[0].value === 'true';
+    const hasSeenTutorial = await response[1].value === 'true';
+    const username = await response[2].value || undefined;
+    const data = {
+      isLoggedin,
+      hasSeenTutorial,
+      username
+    };
+    return data;
+};
+
+export const setHasSeenTutorialData = async (hasSeenTutorial: boolean) => {
+    await Storage.set({ key: HAS_SEEN_TUTORIAL, value: JSON.stringify(hasSeenTutorial) });
+};
+
+export const setIsLoggedInData = async (isLoggedIn: boolean) => {
+    await Storage.set({ key: HAS_LOGGED_IN, value: JSON.stringify(isLoggedIn) });
+};
+
+export const setUsernameData = async (username?: string) => {
+    if (!username) {
+      await Storage.remove({ key: USERNAME });
+    } else {
+      await Storage.set({ key: USERNAME, value: username });
+    }
+};
---
+++ src/data/state.ts
@@ -0,0 +1,25 @@
+import { combineReducers } from './combineReducers';
+import { sessionsReducer } from './sessions/sessions.reducer';
+import { userReducer } from './user/user.reducer';
+
+export const initialState: AppState = {
+  data: {
+    persons: [],
+    filteredPersons: [],
+    loading: false,
+    menuEnabled: true,
+  },
+  user: {
+    hasSeenTutorial: false,
+    darkMode: false,
+    isLoggedin: false,
+    loading: false
+  }
+};
+
+export const reducers = combineReducers({
+  data: sessionsReducer,
+  user: userReducer
+});
+
+export type AppState = ReturnType<typeof reducers>;
