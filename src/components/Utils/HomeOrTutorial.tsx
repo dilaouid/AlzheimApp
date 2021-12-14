b62e230aa9cb7bdaff9a35cf22bbada8e815e5ad
dilaouid <dilaouid@student.42.fr>
2021-12-14 11:25:59 +0100

    (~) Change redirection

--- src/components/Utils/HomeOrTutorial.tsx
+++ src/components/Utils/HomeOrTutorial.tsx
@@ -7,7 +7,7 @@ interface StateProps {
 }
 
 const HomeOrTutorial: React.FC<StateProps> = ({ hasSeenTutorial }) => {
-  return hasSeenTutorial ? <Redirect to="/tabs/schedule" /> : <Redirect to="/tutorial" />
+  return hasSeenTutorial ? <Redirect to="/tabs/tab1" /> : <Redirect to="/tutorial" />
 };
 
 export default connect<{}, StateProps, {}>({
