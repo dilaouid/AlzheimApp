d97b88cbe37e67379d79f693ca41d524b027ad05
dilaouid <dilaouid@student.42.fr>
2022-01-01 15:41:57 +0100

    (+) New background

--- components/utils/Lottie.js
+++ components/utils/Lottie.js
@@ -9,6 +9,7 @@ const Lottie = (props) => {
             source={props.LottieSource}
             loop={props.hasOwnProperty('loop') ? props.loop : true}
             autoPlay={props.hasOwnProperty('autoPlay') ? props.autoPlay : true}
+            resizeMode="cover"
         /> : <Image source={props.ImageSource} style={props.ImageStyle} />
     }
 };
