c5236ed17035f43dd663b28a49715e40103a8042
dilaouid <dilaouid@student.42.fr>
2022-01-17 16:15:43 +0100

    (+) Back button View Quizz List

--- components/activities/Quizz/ViewQuizz.js
+++ components/activities/Quizz/ViewQuizz.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { View } from 'react-native';
 import { Button, Icon, Divider } from 'react-native-elements';
 
 import { lang as QuizzLang } from '../../../language/activities/quizz';
@@ -9,14 +9,25 @@ import styles from './styles';
 export default function ViewQuizz(props) {
   return (
     <>
-      <Button raised title={QuizzLang[props.lang].Create} containerStyle={styles.createButton} icon={
-            <Icon
-            name={"construct-outline"}
-            type={"ionicon"}
-            color={"white"}
-            size={15}
-            style={{marginHorizontal: 5}}
-            /> }/>
+      <View style={{flexDirection: 'row'}}>
+        <Button raised title={QuizzLang[props.lang].Create} containerStyle={styles.createButton} icon={
+              <Icon
+              name={"construct-outline"}
+              type={"ionicon"}
+              color={"white"}
+              size={15}
+              style={{marginHorizontal: 5}}
+              /> }/>
+        <Button title={{}} buttonStyle={{backgroundColor:'red'}} containerStyle={{borderRadius: 15, marginHorizontal: 10, width: 40}} icon={
+              <Icon
+              name={"caret-back-outline"}
+              type={"ionicon"}
+              color={"white"}
+              size={15}
+              /> }
+              onPress={() => props.setTab(0)}
+        />
+      </View>
       <Divider color={'grey'} width={1} style={{width: 100+'%', marginVertical: 20}}  />
     </>
   );
