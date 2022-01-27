42a85decd5fefa935f2c821662fcea3934276536
dilaouid <dilaouid@student.42.fr>
2022-01-27 23:59:31 +0100

    (-) useless imports

--- components/activities/Quiz/Play/index.js
+++ components/activities/Quiz/Play/index.js
@@ -1,17 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
-import { Text } from 'react-native-elements';
 
 import styles from '../styles';
 
 import Menu from './Menu';
 import Game from './Game';
 
+// This component is mounted when the user clicks on 'Play' in the main quiz menu
 export default function Play(props) {
-    const [isLoading, setIsLoading] = useState(true);
-    const [tab, setTab] = useState(0);
     const [play, setPlay] = useState(false);
-    const ThinkingImage = require('../../../../assets/img/activities/quiz/thinking.gif')
 
     return (
     <View style={styles.view}>
