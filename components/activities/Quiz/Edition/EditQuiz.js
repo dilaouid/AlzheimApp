e69bcdb6b812f9461f543f38155eee372ced6c45
dilaouid <dilaouid@student.42.fr>
2022-01-28 16:04:58 +0100

    (+) draft quiz content view + edit question

---
+++ components/activities/Quiz/Edition/EditQuiz.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+
+import styles from '../styles';
+
+import { lang as QuizLang } from '../../../language/activities/quiz';
+
+export default function EditQuiz(props) {
+    return (
+        <View>
+
+        </View>
+    );
+};
---
+++ components/activities/Quiz/Edition/ViewContentQuiz.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+
+import styles from '../styles';
+
+import { lang as QuizLang } from '../../../language/activities/quiz';
+
+export default function ViewContentQuiz(props) {
+    return (
+        <View>
+
+        </View>
+    );
+};
