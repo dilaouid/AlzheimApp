a9665b534f58ddca923f127d2404f4a2b923abc2
dilaouid <dilaouid@student.42.fr>
2022-01-30 22:41:40 +0100

    (-) useless props

--- components/activities/Quiz/Creation/CreateQuiz.js
+++ components/activities/Quiz/Creation/CreateQuiz.js
@@ -165,7 +165,6 @@ export default function CreateQuiz(props) {
                 <FormQuizContent
                     /* Form to create a quiz */
                     lang={props.lang}
-                    setCreateQuestion={setCreateQuestion}
                     setAnswers={setAnswers}
                     setFileType={setFileType}
                     setQuestion={setQuestion}
