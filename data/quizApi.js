a5bc9f8a3f97ad729cd44f2c82f42223310c37dd
dilaouid <dilaouid@student.42.fr>
2022-02-01 23:32:24 +0100

    (-) weird import ?

--- data/quizApi.js
+++ data/quizApi.js
@@ -1,5 +1,4 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { _DEFAULT_PROGRESS_UPDATE_INTERVAL_MILLIS } from 'expo-av/build/AV';
 import * as FileSystem from 'expo-file-system';
 
 import { v4 as uuidv4 } from 'uuid';
