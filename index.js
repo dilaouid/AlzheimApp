a5d745b4836b50d07d78895d732d7e9705e67e28
yedine <dilaouid@student.42.fr>
2022-02-17 19:47:40 +0100

    [+] ignore mobile font size

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { registerRootComponent } from 'expo';
+import React from 'react';
 
+import { Text } from 'react-native'
 import App from './App';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
