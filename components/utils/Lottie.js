848575c19aa256b148c77d8ba6da8d9baf1f2373
dilaouid <dilaouid@student.42.fr>
2022-04-01 22:14:12 +0200

    (~) fix autoplay LottieView for iOS

--- components/utils/Lottie.js
+++ components/utils/Lottie.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import LottieView from 'lottie-react-native';
 import { Platform, Image } from 'react-native';
 
 const Lottie = (props) => {
+    const animationRef = useRef(LottieView);
+
+    useEffect( () => {
+        animationRef?.current?.play();
+    }, []);
+    
     {
         return Platform.OS !== 'web' ? (
             <LottieView
+                ref={animation => {
+                    animationRef.current = animation
+                }}
                 style={props.LottieStyle}
                 source={props.LottieSource}
                 loop={props.hasOwnProperty('loop') ? props.loop : true}
